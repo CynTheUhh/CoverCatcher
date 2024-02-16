760e1301f8def9e1d9b04cef317f626a27ec21ff
Cynthia Chand <137232715+CynTheUhh@users.noreply.github.com>
2024-02-15 20:29:04 -0800

    Commented out javascript to avoid getting banned by the library

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-async function makeApICall() {
-    try {
-        const retrievedData = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
-        //console.log(retrievedData);
-        return retrievedData;    
-    }
-    catch (error) {
-        console.log(error);
-    }
-}
+// async function makeApICall() {
+//     try {
+//         const retrievedData = await fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings");
+//         //console.log(retrievedData);
+//         return retrievedData;    
+//     }
+//     catch (error) {
+//         console.log(error);
+//     }
+// }
 
-async function converttoJSON() {
-    const jsonData = await makeApICall();
-    return jsonData.json();
-}
+// async function converttoJSON() {
+//     const jsonData = await makeApICall();
+//     return jsonData.json();
+// }
 
-const libData = converttoJSON();
-libData.then((res)=>{
-    //res is the whole response
-    //res.docs is a list of book object with lots of information
-    const books =  document.getElementsByClassName("book-card")
-    let apicounter = 0
-    for(let i = 0; i < books.length ;i++){
-        while(!res.docs[apicounter].isbn){
-            apicounter++
-        }
-        let isbn = res.docs[apicounter].isbn[0]
-        apicounter++
-        books[i].style.backgroundImage = "url('https://covers.openlibrary.org/b/isbn/"+isbn+"-L.jpg')";
-    }
-    console.log(res.docs[0].isbn[0])
-})
+// const libData = converttoJSON();
+// libData.then((res)=>{
+//     //res is the whole response
+//     //res.docs is a list of book object with lots of information
+//     const books =  document.getElementsByClassName("book-card")
+//     let apicounter = 0
+//     for(let i = 0; i < books.length ;i++){
+//         while(!res.docs[apicounter].isbn){
+//             apicounter++
+//         }
+//         let isbn = res.docs[apicounter].isbn[0]
+//         apicounter++
+//         books[i].style.backgroundImage = "url('https://covers.openlibrary.org/b/isbn/"+isbn+"-L.jpg')";
+//     }
+//     console.log(res.docs[0].isbn[0])
+// })
 
 
 
